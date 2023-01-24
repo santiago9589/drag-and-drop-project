1486e4ede15c44d741a9a66f7a3043890f00a879
santiago9589 <santiagodejesus587@gmail.com>
2023-01-24 20:04:58 -0300

    drag and drop done with styles

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,14 +4,14 @@ import NavbarComponent from "./components/NavbarComponent"
 import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
 
 const getItemStyle = (isDraggeble: boolean, draggableStyle: any) => ({
-  background: isDraggeble ? "red" : "white",
+  background: isDraggeble ? "green" : "white",
   ...draggableStyle
 }
 )
 
 function App() {
 
-  const rowsContent = new Map<string,Set<string>>()
+  const rowsContent = new Map<string, Set<string>>()
 
   const [state, setState] = useState(rowsContent)
 
@@ -40,10 +40,10 @@ function App() {
     console.log(newOrder)
     arrayToDelete?.delete(newOrder)
 
-    recivedArray.splice(destination.index, 0,newOrder)
+    recivedArray.splice(destination.index, 0, newOrder)
     const setUpdate = new Set(recivedArray)
     draft.delete(destination.droppableId)
-    draft.set(destination.droppableId,setUpdate)
+    draft.set(destination.droppableId, setUpdate)
 
     setState(draft)
   }
@@ -51,21 +51,19 @@ function App() {
   useEffect(() => {
 
     const draft = structuredClone(state)
-
     draft.set('task-0', new Set(["limpiar", "bailar", "comer"]))
     draft.set('task-1', new Set(["salir"]))
     draft.set('task-2', new Set(["dormir"]))
-
     setState(draft)
-
   }, [])
+
 
 
   return (
     <main className="container mx-auto box-border h-[500px]">
       <NavbarComponent />
       <DragDropContext onDragEnd={onDragEnd}>
-        <div style={{ display: "flex" }}>
+        <div className="flex h-[800px] gap-3  border-2 rounded-lg shadow-slate-200 shadow-lg overflow-x-auto p-2">
           {
             Array.from({ length: Array.from(state.keys()).length }, (_, i) => {
 
@@ -78,28 +76,43 @@ function App() {
                       <div
                         {...provided.droppableProps}
                         ref={provided.innerRef}
-                        style={{ padding: "10px", margin: "10px", background: "green" }}
+                        className='card-container'
                       >
-                        {
-                          Array.from(taskArray!).map((task: string, index) => {
-                            return (
-                              <Draggable key={index} draggableId={`taskN-${task}`} index={index}>
-                                {(provided, snapshot) => (
-                                  <div
-                                    {...provided.draggableProps}
-                                    ref={provided.innerRef}
-                                    {...provided.dragHandleProps}
-                                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-                                  >
-                                    {task}
-                                  </div>
-                                )}
+                        <header className="card-header">
+                          <h2 className='text-xl capitalize font-semibold'>{"ramdom-ramdom"}</h2>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                          </svg>
+                        </header>
+                        <article className='overflow-y-auto'>
+                          {
+                            Array.from(taskArray!).map((task: string, index) => {
+                              return (
+                                <Draggable key={index} draggableId={`taskN-${task}`} index={index}>
+                                  {(provided, snapshot) => (
+                                    <div
+                                      {...provided.draggableProps}
+                                      ref={provided.innerRef}
+                                      {...provided.dragHandleProps}
+                                      style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                                      className='card-activity'
+                                    >
+                                      <h2 className='card-activity-text'>{task}</h2>
+                                    </div>
+                                  )}
 
-                              </Draggable>
-                            )
-                          })
-                        }
-                        {provided.placeholder}
+                                </Draggable>
+                              )
+                            })
+                          }
+                          {provided.placeholder}
+                        </article>
+                        <section className='card-bottom'>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                          </svg>
+                          <p>Añadir una tarjeta</p>
+                        </section>
                       </div>
                     )
                   }
